b6a71dd06742e0e989cac3bde25bf411ff0e6df7
JanRK <jk@itmatic.dk>
2021-01-08 02:36:43 +0100

    Function specific messages

--- Rednet/rednet.lua
+++ Rednet/rednet.lua
@@ -23,7 +23,7 @@ if rednetRole == "server" then
     rednet.host(rednetProtocol,rednetHostname)
     while true do
         local senderId, message, protocol = rednet.receive(rednetProtocol)
-        local receivedMessage = message
+        local receivedMessage = textutils.unserialiseJSON(message)
         print("ID: " .. senderId .. " Message: " .. message)
     end
 elseif rednetRole == "client" then
@@ -34,8 +34,18 @@ elseif rednetRole == "client" then
     x, y, z = gps.locate(3)
 
     local messageToSend = "Ping"
+
     if args[2] then
         messageToSend = args[2]
+        if args[2] == "miner" then
+            local progressInFile = fs.open("/progress", "r" )
+            local progressFromFile = progressInFile.readLine()
+            progressInFile.close()
+            messageToSend = {
+                Function = "Miner",
+                MiningProgress = progressFromFile,
+                Fuel = tostring(turtle.getFuelLevel())
+            }
     end
 
     local myTable = {
@@ -51,4 +61,8 @@ elseif rednetRole == "client" then
 else
     print("No role set, call with server or client param")
 end
+end
 
+--table1 = { test1 = "test1", test2 = "test2"}
+--table2 = { test3 = "test3", test4 = "test4"}
+--table3 = { test5 = "test5", test6 = table2}
