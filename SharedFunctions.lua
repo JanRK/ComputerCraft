f62950ff8f983eeda98f6edf8117b892f9a1b0b2
Jan Knudsen <jk@itmatic.dk>
2022-01-23 17:54:41 +0100

    Add some functions

--- SharedFunctions.lua
+++ SharedFunctions.lua
@@ -68,3 +68,58 @@ end
 -- split_string[2] = "World!"
 -- print(split_string[1])
 -- print(split_string[2])
+
+function listItems()
+  local result = {}
+  for i=1,16 do
+    local item = turtle.getItemDetail(i)
+    if item then
+      local itemName = item["name"]
+      local itemCount = turtle.getItemCount(i)
+      result[i] = itemCount .. " " .. itemName
+    end
+  end
+  return result
+end
+
+function findItem(itemName)
+  for i=1,16 do
+    local item = turtle.getItemDetail(i)
+    if item then
+      if (turtle.getItemDetail(i)["name"] == string.lower(itemName)) then
+        return i
+      end
+    end
+  end
+  return -1
+end
+
+function findDeviceSide(deviceType)
+  local listSides = {"left","right","up","down","front","back","top","bottom"};
+  for i, side in pairs(listSides) do
+    if (peripheral.isPresent(side)) then
+      if (peripheral.getType(side) == string.lower(deviceType)) then
+        return side;
+      end
+    end
+  end -- for-do
+  return nil;
+end
+
+function oppositeSide(side)
+  if (side == "left") then
+    return "right"
+  elseif (side == "right") then
+    return "left"
+  elseif (side == "up") then
+    return "down"
+  elseif (side == "down") then
+    return "up"
+  elseif (side == "front") then
+    return "back"
+  elseif (side == "back") then
+    return "front"
+  else
+    return nil
+  end
+end
